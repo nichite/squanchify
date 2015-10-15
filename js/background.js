0747e97c9dc5c6d1523497bbfaedb4ce66ecd862
Nic Hite <nichite.gmail.com>
2015-10-15 01:55:25 -0500

    Remove redundant storage reads, update UI

--- js/background.js
+++ js/background.js
@@ -7,42 +7,45 @@ $(document).ready(function(){
     loadSquanchPrefs();
 
     $(".squanchy-button").on("click", function(){
-        tryGetStorageValue('activated', deOrReSquanch);
+        var activated = $.parseJSON($(this).attr('activated'));
+        setSquanchButton(activated, true);
     });
 
     $(".squanchy-slider").on("change", function(){
         var sliderVal = this.value;
-        updateSquanchiness(mapSliderToSquanchiness(sliderVal));
+        updateSquanchiness(mapSliderToSquanchiness(sliderVal), true);
     });
 });
 
 loadSquanchPrefs = function() {
-    tryGetStorageValue('squanchiness', setSquanchySlider);
-    tryGetStorageValue('activated', setSquanchButton);
+    tryGetStorageValue('squanchiness', setSquanchySlider, false);
+    tryGetStorageValue('activated', setSquanchButton, false);
 };
 
-setSquanchySlider = function(squanchiness){
+setSquanchySlider = function(squanchiness, reloadTabInd){
     $(".squanchy-slider").val(mapSquanchinessToSlider(squanchiness));
+
+    if (reloadTabInd)
+        updateSquanchiness(squanchiness, reloadTabInd);
 };
 
-setSquanchButton = function(activated) {
-    var squanchButton = $(".squanchy-button");
-    if (activated === true) {
-        squanchButton.html("De-Squanch");
-    } else {
-        squanchButton.html("Re-Squanch");
-    }
-};
+setSquanchButton = function(currentlyActivated, reloadTabInd) {
+    var setToActivated = ((currentlyActivated && !reloadTabInd) || (!currentlyActivated && reloadTabInd));
 
-deOrReSquanch = function(activated) {
     var squanchButton = $(".squanchy-button");
-    if (activated === true) {
-        squanchButton.html("Re-Squanch");
-        chrome.storage.sync.set({'activated': false}, reloadCurrentTab);
-    } else {
-        squanchButton.html("De-Squanch");
-        chrome.storage.sync.set({'activated': true}, reloadCurrentTab);
-    }
+    var squanchSlider = $(".slider-container");
+
+    squanchButton.html(setToActivated ? "De-Squanch" : "Re-Squanch");
+    squanchButton.css("background", (setToActivated ? "#e64343" : "#0ada8f"));
+    squanchButton.attr("activated", (setToActivated ? true : false));
+
+    if (setToActivated)
+        squanchSlider.slideDown(500);
+    else
+        squanchSlider.slideUp(500);
+
+    if (reloadCurrentTab)
+        updateActivated(setToActivated, reloadTabInd);
 };
 
 reloadCurrentTab = function() {
@@ -51,22 +54,28 @@ reloadCurrentTab = function() {
     });
 };
 
-tryGetStorageValue = function(inputVal, setFunction) {
+tryGetStorageValue = function(inputVal, setFunction, reloadTabInd) {
     return chrome.storage.sync.get(inputVal, function(data) {
         if (data[inputVal] === undefined) {
             data[inputVal] = defaults[inputVal];
-            chrome.storage.sync.set({inputVal: data[inputVal]});
         }
-        setFunction(data[inputVal]);
+        setFunction(data[inputVal], reloadTabInd);
     });
 };
 
-updateSquanchiness = function(squanchiness) {
-    chrome.storage.sync.set({'squanchiness': squanchiness}, reloadCurrentTab);
+updateSquanchiness = function(squanchiness, reloadTabInd) {
+    if (reloadTabInd)
+        chrome.storage.sync.set({'squanchiness': squanchiness}, reloadCurrentTab);
+    else {
+        chrome.storage.sync.set({'squanchiness': squanchiness});
+    }
 };
 
-updateActivated = function(activated) {
-    chrome.storage.sync.set({'activated': activated}, reloadCurrentTab);
+updateActivated = function(activated, reloadTabInd) {
+    if (reloadTabInd)
+        chrome.storage.sync.set({'activated': activated}, reloadCurrentTab);
+    else
+        chrome.storage.sync.set({'activated': activated});
 };
 
 mapSliderToSquanchiness = function(sliderVal) {
